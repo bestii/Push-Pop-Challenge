4d63356602937066abf70e00515162a88a51fb02
bestin-john <bestinjohn1993@gmail.com>
2021-03-17 10:54:59 +0100

    Updated title tag

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -3,7 +3,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Defect Management' || process.env.npm_package_name,
     meta: [{
         charset: 'utf-8'
       },
