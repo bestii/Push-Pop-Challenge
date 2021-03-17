2e276aea5cc811282dd507dfedcdf69ac5e035ce
bestin-john <bestinjohn1993@gmail.com>
2021-03-17 11:09:48 +0100

    Added profile icon and adjusted shadow

--- frontend/components/AppHeader.vue
+++ frontend/components/AppHeader.vue
@@ -1,12 +1,19 @@
 <template>
   <header
-    class="top-0 w-full z-40 bg-white fixed border-b border-gray-200 shadow"
+    class="top-0 w-full z-40 bg-white fixed border-b border-gray-200 shadow-lg"
   >
     <div class="flex items-center justify-between h-16 px-6 mx-auto">
       <a href="/">
         <img
           alt="header logo"
           src="../static/nuxt-logo.svg"
+          class="h-8 sm:w-8 lg:w-auto"
+        />
+      </a>
+      <a href="/">
+        <img
+          alt="User Profile"
+          src="../static/icons/user_icon.webp"
           class="h-8 sm:w-8 lg:w-auto"
         />
       </a>
