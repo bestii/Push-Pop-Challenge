fcb427af2b703e930caa847414381f7da99180bf
bestin-john <bestinjohn1993@gmail.com>
2021-03-17 23:37:00 +0100

    Added header scope

--- frontend/components/ListTable.vue
+++ frontend/components/ListTable.vue
@@ -1,18 +1,24 @@
 <template>
-  <table class="min-w-full leading-normal list-table">
+  <table
+    class="min-w-full leading-normal list-table"
+    aria-describedby="list of tickets"
+  >
     <thead>
       <tr class="border-b-2 border-gray-300">
         <th
+          scope="col"
           class="px-5 py-3 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
         >
           Key
         </th>
         <th
+          scope="col"
           class="px-5 py-3 border-b-2 border-gray-300 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
         >
           Description
         </th>
         <th
+          scope="col"
           class="px-5 py-3 border-b-2 border-gray-300 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
         ></th>
       </tr>
@@ -71,5 +77,4 @@ export default {
 .list-table > tbody > tr:last-child {
   border-bottom: none;
 }
-
 </style>
