4bf55d770cbc967b7b40c4fd6cd89b963dc3b99d
bestin-john <bestinjohn1993@gmail.com>
2021-03-17 19:38:35 +0100

    Implemented tabbed structure for displaying tickets

--- frontend/pages/script.js
+++ frontend/pages/script.js
@@ -43,9 +43,98 @@ export default {
       resolved: [],
       unresolved: [],
       backlog: [],
+      activetab: 1,
       LIST_BACKLOG,
       LIST_RESOLVED,
       LIST_UNRESOLVED,
+      //{time_stamp, initial_list, final_list, item, code}
+      logArray: [],
     };
+  },
+  computed: {
+    resolvedLen: function () {
+      return this.resolved.length;
+    },
+    unresolvedLen: function () {
+      return this.unresolved.length;
+    },
+    backlogLen: function () {
+      return this.backlog.length;
+    },
+    totalTickets: function () {
+      return (this.backlogLen + this.unresolvedLen + this.resolvedLen);
+    },
+    backlogPercent: function () {
+      return ((this.backlogLen / this.totalTickets * 100).toFixed(2) + '%');
+    },
+    unresolvedPercent: function () {
+      return ((this.unresolvedLen / this.totalTickets * 100).toFixed(2) + '%');
+    },
+    resolvedPercent: function () {
+      return ((this.resolvedLen / this.totalTickets * 100).toFixed(2) + '%');
+    },
+    sortedResolved: function () {
+      return this.resolved.sort((a, b) => {
+        if (a.code < b.code) return -1;
+        if (a.code > b.code) return 1;
+        return 0;
+      });
+    },
+    sortedUnresolved: function () {
+      return this.unresolved.sort((a, b) => {
+        if (a.code < b.code) return -1;
+        if (a.code > b.code) return 1;
+        return 0;
+      });
+    },
+    sortedbacklog: function () {
+      return this.backlog.sort((a, b) => {
+        if (a.code < b.code) return -1;
+        if (a.code > b.code) return 1;
+        return 0;
+      });
+    }
+  },
+  methods: {
+    moveTickets(ticket) {
+      console.log(ticket);
+      switch (ticket.type) {
+        case this.LIST_RESOLVED:
+          this.createLog(Date.now(), this.LIST_RESOLVED, this.LIST_UNRESOLVED, ticket.item, ticket.item.code);
+          this.resolved = this.resolved.filter(item => item.code != ticket.item.code);
+          this.unresolved.push(ticket.item);
+          break;
+        case this.LIST_UNRESOLVED:
+          this.createLog(Date.now(), this.LIST_UNRESOLVED, this.LIST_RESOLVED, ticket.item, ticket.item.code);
+          this.unresolved = this.unresolved.filter(item => item.code != ticket.item.code);
+          this.resolved.push(ticket.item);
+          break;
+        case this.LIST_BACKLOG:
+          this.createLog(Date.now(), this.LIST_BACKLOG, this.LIST_UNRESOLVED, ticket.item, ticket.item.code);
+          this.backlog = this.backlog.filter(item => item.code != ticket.item.code);
+          this.unresolved.push(ticket.item);
+          break;
+      }
+    },
+    createLog(time_stamp, initial_list, final_list, item, code) {
+
+      const log = {
+        time_stamp,
+        initial_list,
+        final_list,
+        item,
+        code
+      }
+      this.logArray.pop();
+      this.logArray.push(log)
+    },
+    undoChange() {
+      const log = this.logArray.pop();
+      const ticket = {
+        type: log.final_list,
+        item: log.item
+      }
+      this.moveTickets(ticket);
+    }
   },
 };
