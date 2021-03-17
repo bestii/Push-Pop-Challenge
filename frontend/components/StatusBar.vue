ca100a9f8a9b7702d78a73df98efbe3d0f205bc3
bestin-john <bestinjohn1993@gmail.com>
2021-03-17 16:51:57 +0100

    Added title to status bar

--- frontend/components/StatusBar.vue
+++ frontend/components/StatusBar.vue
@@ -2,15 +2,18 @@
   <div class="relative pt-1">
     <div class="overflow-hidden h-2 text-xs flex rounded bg-yellow-200">
       <div
-        v-bind:style="{ width: resolvedPercent }"
+        :title="'Resolved: ' + resolvedPercent"
+        :style="{ width: resolvedPercent }"
         class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-600"
       ></div>
       <div
-        v-bind:style="{ width: unresolvedPercent }"
+        :title="'Unresolved: ' + unresolvedPercent"
+        :style="{ width: unresolvedPercent }"
         class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-600"
       ></div>
       <div
-        v-bind:style="{ width: backlogPercent }"
+        :title="'Backlog: ' + backlogPercent"
+        :style="{ width: backlogPercent }"
         class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gray-400"
       ></div>
     </div>
